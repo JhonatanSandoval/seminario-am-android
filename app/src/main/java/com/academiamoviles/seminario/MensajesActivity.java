82ea51bd522a0356280aa6dc11402e67babc0c62
Jhonatan S <jsandoval@touchsf.net>
2017-05-30 21:15:50 -0500

    enviando y recibiendo mensajes, con socket

--- app/src/main/java/com/academiamoviles/seminario/MensajesActivity.java
+++ app/src/main/java/com/academiamoviles/seminario/MensajesActivity.java
@@ -1,10 +1,14 @@
 package com.academiamoviles.seminario;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,6 +16,9 @@ import com.academiamoviles.seminario.adapter.MensajesAdapter;
 import com.academiamoviles.seminario.model.MensajeModel;
 import com.academiamoviles.seminario.services.GeolocationService;
 import com.academiamoviles.seminario.services.SocketService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +32,33 @@ public class MensajesActivity extends AppCompatActivity {
 
     List<MensajeModel> mLista = new ArrayList<>();
     MensajesAdapter adapter;
+
+
+    BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            switch (action) {
+                case "NUEVO_MENSAJE":
+
+                    // insertar el mensaje en la lista (mLista)
+                    String nombres = intent.getStringExtra("nombres");
+                    String mensaje = intent.getStringExtra("mensaje");
+
+                    MensajeModel obj = new MensajeModel();
+                    obj.setNombres(nombres);
+                    obj.setMensaje(mensaje);
+
+                    mLista.add(obj);
+
+                    cargarListaMensajes();
+
+                    break;
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +75,21 @@ public class MensajesActivity extends AppCompatActivity {
 
         initViews();
         setupRecyclerView();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // registrar el broadcastreceiver
+
+        IntentFilter mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction("NUEVO_MENSAJE");
+        mIntentFilter.addAction("OTRO_MENSAJE");
+        mIntentFilter.addAction("LISTA_MENSAJES");
+
+        registerReceiver(mBroadcastReceiver, mIntentFilter);
 
     }
 
@@ -61,5 +110,34 @@ public class MensajesActivity extends AppCompatActivity {
 
         btnGrabar = (Button) findViewById(R.id.btnGrabar);
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombres = etNombres.getText().toString();
+                String mensaje = etMensaje.getText().toString();
+
+                if (!nombres.isEmpty() && !mensaje.isEmpty()) {
+
+                    // emitir al servidor
+
+                    JSONObject json = new JSONObject();
+                    try {
+                        json.put("nombres", nombres);
+                        json.put("mensaje", mensaje);
+
+                        SocketService.emitirEvento("enviar_mensaje", json);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                }
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/academiamoviles/seminario/services/SocketService.java
+++ app/src/main/java/com/academiamoviles/seminario/services/SocketService.java
@@ -9,9 +9,13 @@ import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SocketService extends Service {
 
-    private Socket mSocket;
+    private static Socket mSocket;
     {
         try {
             mSocket = IO.socket("http://192.168.1.22:9000");
@@ -31,7 +35,49 @@ public class SocketService extends Service {
                 Log.i("SocketService", "datos recibidos");
             }
         });
+        mSocket.on("nuevo_mensaje", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                JSONObject datos = (JSONObject) args[0];
+                // obtener los datos de args[0]
 
+                try {
+                    String nombres = datos.getString("nombres");
+                    String mensaje = datos.getString("mensaje");
+
+
+                    Intent iBroadcast = new Intent();
+                    iBroadcast.putExtra("nombres", nombres);
+                    iBroadcast.putExtra("mensaje", mensaje);
+
+                    iBroadcast.setAction("NUEVO_MENSAJE");
+                    sendBroadcast(iBroadcast);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                emitirEvento("listar_mensajes", null);
+            }
+        });
+        mSocket.on("mensajes", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                JSONArray array = (JSONArray) args[0];
+                Log.i("SocketService", "array: " + array.toString());
+            }
+        });
+    }
+
+    public static void emitirEvento(String evento, JSONObject json) {
+        if (mSocket.connected()) {
+            mSocket.emit(evento, json);
+        }
     }
 
     public SocketService() {
